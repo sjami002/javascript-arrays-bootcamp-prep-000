cf6c1eff905946f35b237b6064725063a9f88ad0
Shoptorshi Jamil <sjami002@gmail.com>
2019-02-17 20:33:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 var candyStr = "peanuts"
+function addElementToBeginningOfArray(chocolateBars, candyStr) {
+  return [candyStr, ...chocolateBars]
+}
